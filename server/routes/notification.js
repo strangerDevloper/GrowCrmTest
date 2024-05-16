0c488358ae0e5897a41510292760f1337ca7b6b8
frank <frankbusiness411@gmail.com>
2024-05-16 09:59:04 -0700

    test

---
+++ server/routes/notification.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import loggers from "node-loggers"
+
+import { getNotification, getNotifications,  createRequestNotification, deleteNotification, deleteWholeCollection } from '../controllers/notification.js'
+
+const router = express.Router()
+loggers()
+
+// GET
+router.get('/get/single/:notificationId', getNotification)
+router.get('/get/all', getNotifications)
+
+// POST
+router.post('/create/request', createRequestNotification)
+
+// DELETE
+router.delete('/delete/:notificationId', deleteNotification)
+router.delete('/delete/whole-collection', deleteWholeCollection)
+
+export default router
